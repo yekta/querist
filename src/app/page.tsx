4a46076c08008b2a84bc5ec4fdb95ff7d50619f3
yekta <ngyekta@gmail.com>
2023-11-19 18:22:55 +0300

    Navbar tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import { useTheme } from "@components/providers/ThemeProvider";
-import { useDarkMode } from "usehooks-ts";
 
 export default function HomePage() {
   const { theme, systemTheme } = useTheme();
   return (
-    <div className="w-full flex flex-1 items-center justify-center">
-      Home, {theme}, {systemTheme}
+    <div className="w-full flex flex-1 items-stretch justify-center">
+      <div className="w-64 flex flex-col items-start justify-start border-r border-c-outline">
+        <div className="w-full border-b border-c-outline">
+          <p className="px-4 py-3 font-black text-xl">Table Editor</p>
+        </div>
+        <div className="w-full flex flex-col">
+          <p className="px-4 py-2 font-bold text-lg">Schema</p>
+        </div>
+      </div>
+      <div className="flex-1 p-4">
+        Home, {theme}, {systemTheme}
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "@tanstack/react-router";
 
 export default function Navbar() {
   return (
-    <div className="w-full flex items-center justify-center ring-1 ring-c-outline">
+    <div className="w-full flex items-center justify-center border-b border-c-outline">
       <div className="w-full flex items-center justify-between">
         <Link to="/" className="p-2.5">
           <BrandMark />
